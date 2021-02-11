3a2c5ab2d2aac5537afdf9fe9ba6a636a1c53077
Rowan-Mather <76477727+Rowan-Mather@users.noreply.github.com>
2021-02-11 18:59:36 +0000

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,14 +6,19 @@ import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+        //loads the dates on which information is stored
+        prefs.readDates()
+
         //loads the graphical layout
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,26 +37,74 @@ class MainActivity : AppCompatActivity() {
                 R.id.calendar -> {
                     startActivity(Intent(this, Calendar::class.java))
                 }
+                R.id.graphs -> {
+                    startActivity(Intent(this, Graphs::class.java))
+                }
             }
             overridePendingTransition(0,0)
             true
         }
 
-        //initialises the variable spoons and default sets it to 10
-        //  if the number of spoons for that day is stored, it is loaded
-        var spoons = 10
         var currentDate = prefs.getCurrentDate()
+
+        //initialises the variable spoons and the maximum number of spoons
+        //if the number of spoons for that day is stored, it is loaded
+        var spoons: Int
+        var maxSpoons = 10
+        var maxSpoonString = prefs.readSetting("MAXSPOONS")
+        if (maxSpoonString != "ERROR: DATA NOT FOUND") { maxSpoons = maxSpoonString.toInt() }
         var spoonsString = prefs.readData(currentDate,"activity","spoons")
         if (spoonsString != "ERROR: DATA NOT FOUND") { spoons = spoonsString.toInt() }
-        //the text view that displays the number of spoons is updated
-        val spoonCountView = findViewById(R.id.spoonCount) as TextView
-        spoonCountView.text = spoons.toString()
+        else { spoons = maxSpoons }
+        //the text views that display the number of spoons and the maximum number of spoons are updated
+        spoonCount.text = spoons.toString()
+        spoonsDefaultView.text = "Maximum spoons: $maxSpoons"
 
         //when the user taps the spoon button, the spoon count is decremented and the stored data is updated
         spoonButton.setOnClickListener {
-            spoons -= 1
-            spoonCountView.text = spoons.toString()
-            prefs.writeData(currentDate,"activity", "spoons", spoons.toString())
+            if (spoons > 0){
+                spoons -= 1
+                spoonCount.text = spoons.toString()
+                prefs.writeData(currentDate,"activity", "spoons", spoons.toString())
+            }
+            else
+            { Toast.makeText(applicationContext,"You have run out of spoons", Toast.LENGTH_SHORT).show() }
+        }
+
+        //controls the +/- buttons for changing the maximum number of spoons the user has
+        maxSpoonsMinus.setOnClickListener{
+            if (maxSpoons > 1)
+            {
+                maxSpoons -= 1
+                prefs.writeSetting("MAXSPOONS", maxSpoons.toString())
+                if (spoons > 0)
+                {
+                    spoons -= 1
+                    spoonCount.text = spoons.toString()
+                    if (prefs.readData(currentDate,"activity", "spoons") != "ERROR: DATA NOT FOUND")
+                    { prefs.writeData(currentDate,"activity", "spoons", spoons.toString()) }
+                }
+            }
+            else
+            { Toast.makeText(applicationContext,"Your maximum number of spoons can only be from 1-99", Toast.LENGTH_SHORT).show()}
+        }
+        maxSpoonsPlus.setOnClickListener{
+            if (maxSpoons < 99)
+            {
+                maxSpoons += 1
+                prefs.writeSetting("MAXSPOONS", maxSpoons.toString())
+                spoons += 1
+                spoonCount.text = spoons.toString()
+                if (prefs.readData(currentDate,"activity", "spoons") != "ERROR: DATA NOT FOUND")
+                { prefs.writeData(currentDate,"activity", "spoons", spoons.toString()) }
+            }
+            else
+            { Toast.makeText(applicationContext,"Your maximum number of spoons can only be from 1-99", Toast.LENGTH_SHORT).show()}
+        }
+
+        //help button triggers help screen
+        helpButton.setOnClickListener {
+            startActivity(Intent(this, ActivityHelp::class.java))
         }
     }
 }
