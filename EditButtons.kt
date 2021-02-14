474d81b917fd5f162bd9f246585190573df34310
Rowan-Mather <76477727+Rowan-Mather@users.noreply.github.com>
2021-02-14 23:00:41 +0000

    Update EditButtons.kt

--- EditButtons.kt
+++ EditButtons.kt
@@ -2,12 +2,19 @@ package com.example.aboutme
 
 import android.annotation.TargetApi
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.edit_buttons.view.*
 
 class EditButtons : ConstraintLayout {
+    private lateinit var range: MutableList<String>
+    private lateinit var selected: String
+    private var title = ""
     @JvmOverloads
     constructor(
         context: Context?,
@@ -26,8 +33,63 @@ class EditButtons : ConstraintLayout {
     ) : super(context!!, attrs, defStyleAttr, defStyleRes) {
         init()
     }
-
     private fun init() {
         View.inflate(context, R.layout.edit_buttons, this)
+
+        editAdd.setOnClickListener{
+            editButtonClicked(1)
+        }
+        editDelete.setOnClickListener{
+            editButtonClicked(-1)
+        }
     }
+
+    fun setText(text: String)
+    {
+        editButtonTextView.text = text
+    }
+    fun setTitle(text: String)
+    {
+        title = text
+    }
+    fun setButtonColour(hex: String)
+    {
+        editAdd.backgroundTintList = ColorStateList.valueOf(Color.parseColor(hex))
+        editDelete.backgroundTintList = ColorStateList.valueOf(Color.parseColor(hex))
+    }
+    fun setRange(min: Int, max: Int)
+    {
+        range.clear()
+        for (i in min ..  max)
+        {
+            range.add(i.toString())
+        }
+    }
+    fun setRange(completeRange: Array<String>)
+    {
+        range = completeRange.toMutableList()
+    }
+    fun editButtonClicked(change: Int)
+    {
+        if (range != null)
+        {
+            if (selected == null)
+            {
+                selected = if (change > 0) { range[0] }
+                else { range[range.lastIndex] }
+            }
+            else
+            {
+                val newIndex: Int = range.indexOf(selected) + change
+                if (newIndex < 0 || newIndex > range.lastIndex)
+                {
+                    Toast.makeText(context,"Outside the range", Toast.LENGTH_SHORT).show()
+                }
+                else
+                { selected = range[newIndex] }
+            }
+            setText("$title: $selected")
+        }
+    }
+
 }
