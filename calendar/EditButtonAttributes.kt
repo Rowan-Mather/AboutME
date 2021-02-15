c88e1f5bcc47cff6beb76f24a93c44889e637814
Rowan-Mather <76477727+Rowan-Mather@users.noreply.github.com>
2021-02-15 16:59:09 +0000

    Update EditButtonAttributes.kt

--- calendar/EditButtonAttributes.kt
+++ calendar/EditButtonAttributes.kt
@@ -2,10 +2,15 @@ package com.example.aboutme
 
 import kotlin.properties.Delegates
 
+//this class stores attribute data e.g. spoons have range 0-15
+//objects of this class are used to set up the compound view of the text view & +/- buttons in the calendar date activity
 data class EditButtonAttributes(var name: String) {
     var minimum = 0
     var maximum = 0
     var range: Array<String>? = null
+    //the range of the attributes in calendar date can either be numeric e.g. Spoons: 1-10
+    // or strings e.g. Sleep Quality: Poor, Fine, Good
+    //the constructors set the objects up accordingly, and the function isIntRange tells you which type the object is
     constructor(name: String, min: Int, max: Int) : this(name) {
         minimum = min
         maximum = max
