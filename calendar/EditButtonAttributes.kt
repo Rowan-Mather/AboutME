825869e76bc0c0a782d5b75efa6fd5d078a4de62
Rowan-Mather <76477727+Rowan-Mather@users.noreply.github.com>
2021-02-16 19:01:33 +0000

    Update EditButtonAttributes.kt

--- calendar/EditButtonAttributes.kt
+++ calendar/EditButtonAttributes.kt
@@ -8,20 +8,26 @@ data class EditButtonAttributes(var name: String) {
     var minimum = 0
     var maximum = 0
     var range: Array<String>? = null
+    var type: String = ""
+    init {
+        type = "list"
+    }
     //the range of the attributes in calendar date can either be numeric e.g. Spoons: 1-10
     // or strings e.g. Sleep Quality: Poor, Fine, Good
     //the constructors set the objects up accordingly, and the function isIntRange tells you which type the object is
     constructor(name: String, min: Int, max: Int) : this(name) {
         minimum = min
         maximum = max
+        type = "intRange"
     }
     constructor(name: String, scrollRange: Array<String>): this(name)
     {
         range = scrollRange
+        type = "stringRange"
     }
     fun isIntRange(): Boolean
     {
-        if (range == null)
+        if (type == "intRange")
         {
             return true
         }
